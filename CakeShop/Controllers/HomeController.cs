095452b9f8b9994369ebecc50ec9b98bc35143ec
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2020-08-16 18:04:37 +0300

    - Partial Views added (_Carousel , _PieCard)
    - added Views (Index , List )
    
    - HomeController added
    
    - new ViewModel added (HomeViewModel)

---
+++ CakeShop/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CakeShop.Models;
+using CakeShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CakeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                PiesOfTheWeek = _pieRepository.PiesOfTheWeek
+            };
+
+            return View(homeViewModel);
+        }
+      
+    }
+}
