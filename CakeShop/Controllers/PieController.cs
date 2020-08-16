c0ad79a64e552b07aa4f7b7c602f633266008def
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2020-08-16 17:06:11 +0300

    Seed Data
    Migrations
    
    IActionResult Details (id) method in Pie controller

--- CakeShop/Controllers/PieController.cs
+++ CakeShop/Controllers/PieController.cs
@@ -19,5 +19,13 @@ namespace CakeShop.Controllers
         }
 
 
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if (pie == null)
+                return NotFound();
+
+            return View(pie);
+        }
     }
 }
