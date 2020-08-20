efa1b1219e154425cccac3b053f5b3c04ff7c469
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2020-08-20 11:12:48 +0300

    - Added two ViewComponent with their corresponding Views CategoryMenu : ViewComponent ShoppingCartSummary : ViewComponent
    -Added ViewResult method to the PieController
    
    -Added the created ViewComponents to the layaout

--- CakeShop/Controllers/PieController.cs
+++ CakeShop/Controllers/PieController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CakeShop.Models;
+using CakeShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CakeShop.Controllers
@@ -26,6 +27,30 @@ namespace CakeShop.Controllers
                 return NotFound();
 
             return View(pie);
+        }
+
+        public ViewResult List(string category)
+        {
+            IEnumerable<Pie> pies;
+            string currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+
+            return View(new PiesListViewModel
+            {
+                Pies = pies,
+                CurrentCategory = currentCategory
+            });
         }
     }
 }
